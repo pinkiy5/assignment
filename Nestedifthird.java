62d65db5d80e28e9a62701798fa55092df04e2d7
pinkiy5 <122789464+pinkiy5@users.noreply.github.com>
2023-01-24 17:17:32 +0530

    Add files via upload

---
+++ Nestedifthird.java
@@ -0,0 +1,19 @@
+class Nestedifthird{
+public static void main(String[] args){
+
+int num1=2;
+int num2=3;
+
+if(num1==3){
+if(num2==5){
+System.out.println("wrong statement");
+}
+else{
+System.out.println("not matched");
+}
+}
+else{
+System.out.println("first condition not satisfied");
+}
+}
+}
