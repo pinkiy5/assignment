c722a1ae5606c6162895a82219c13886c1ce3b26
pinkiy5 <122789464+pinkiy5@users.noreply.github.com>
2023-02-01 16:25:12 +0530

    Add files via upload

---
+++ SumOfFirst10_NturalNo.java
@@ -0,0 +1,13 @@
+package controlstatement;
+
+public class SumOfFirst10_NturalNo {
+	public static void main(String[] args)   
+	{  
+	int sum = 0;  
+	for(int i = 1; i <= 10; i++) //loop for condition until true 
+	{   
+	sum = sum + i;  
+	}                                                                   
+	System.out.println("Sum of First 10 Natural Numbers is = " + sum);   //prints the sum 
+	}  
+	}
